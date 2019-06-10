76d34d94b9e97886901e1bdef3cf16107073fd7b
pwhauman <Peter.Hauman@bottomline.tech>
2019-06-10 12:56:19 -0400

    added .jar file for normal distribution

---
+++ SyntheticData/src/main/java/com/bottomline/SyntheticData/GaussDist.java
@@ -0,0 +1,28 @@
+package com.bottomline.SyntheticData;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+public class GaussDist {
+    private double mean;
+    private double standarDev;
+    private NormalDistribution gaussDist;
+
+
+    public GaussDist(double mean, double standarDev) {
+        this.mean = mean;
+        this.standarDev = standarDev;
+        gaussDist = new NormalDistribution(mean, standarDev);
+    }
+
+    public double generateSample() {
+        return gaussDist.sample;
+    }
+
+    public double[] generateSample(int sampleSize) {
+        double[] sample = new double[sampleSize];
+        for (int i = 0; i < sampleSize; i++) {
+            sample[i] = generateSample();
+        }
+        return sample;
+    }
+}
