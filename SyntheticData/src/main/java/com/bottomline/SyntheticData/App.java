8d1a76bd93e6b458c48aee2ae504175b163a7fd0
ejsealBT <Ethan.Seal@bottomline.tech>
2019-06-10 09:45:17 -0400

    Made a new Maven Project

---
+++ SyntheticData/src/main/java/com/bottomline/SyntheticData/App.java
@@ -0,0 +1,13 @@
+package com.bottomline.SyntheticData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
