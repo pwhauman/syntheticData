daca9d3924faac20f767e9712dd6740018174589
ejsealBT <Ethan.Seal@bottomline.tech>
2019-06-10 11:27:17 -0400

    Added code for exponential and polynomial functions

--- SyntheticData/src/main/java/com/bottomline/SyntheticData/Exp.java
+++ SyntheticData/src/main/java/com/bottomline/SyntheticData/Exp.java
@@ -1,5 +1,31 @@
 package com.bottomline.SyntheticData;
 
-public class Exp {
+/**
+ * @author ethan.seal
+ * This class handles all exponential functions in the form y = <base>^x
+ */
 
+public class Exp {
+	private double base;
+	private double val;
+	private boolean compute;
+	
+	public Exp(double base) {
+		this.base = base;
+	}
+
+	//gets the previously computed value
+	public double getVal(double x) {
+		if (!this.compute) {
+			this.val = compute(x);
+		}
+		return this.val;
+	}
+	
+	
+	//computes the value of the function for a given x
+	private double compute(double x) {
+		this.compute = true;
+		return Math.pow(this.base, x);
+	}
 }
--- SyntheticData/src/main/java/com/bottomline/SyntheticData/Poly.java
+++ SyntheticData/src/main/java/com/bottomline/SyntheticData/Poly.java
@@ -1,7 +1,34 @@
 package com.bottomline.SyntheticData;
 
+/**
+ * @author ethan.seal
+ * This class handles all polynomial functions in the form y = <coeff>x^<exp>
+ */
+
 public class Poly {
-	public Poly(double x, double coeff, double exp) {
+	private double exp;
+	private double coeff;
+	private double val;
+	private boolean compute;
 	
+	public Poly(double coeff, double exp) {
+		this.coeff = coeff;
+		this.exp = exp;
+	}
+	
+	//this function returns the previously computed value 
+	public double getVal(double x) {
+		if (!this.compute) {
+			this.val = compute(x);
+		}
+		return this.val;
+	}
+	
+	//computes the value of the function for a given x
+	private double compute(double x) {
+		this.compute = true;
+		this.val = (this.coeff * Math.pow(x, this.exp));
+		return this.val;
 	}
 }
+
